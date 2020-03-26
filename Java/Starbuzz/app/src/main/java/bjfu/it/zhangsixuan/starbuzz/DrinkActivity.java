3c0c68568095b25f3ed45268576be225c7163fa3
christy <christy99cc@qq.com>
2020-03-26 15:02:10 +0800

    starbuzz 增加数据库

--- Java/Starbuzz/app/src/main/java/bjfu/it/zhangsixuan/starbuzz/DrinkActivity.java
+++ Java/Starbuzz/app/src/main/java/bjfu/it/zhangsixuan/starbuzz/DrinkActivity.java
@@ -2,9 +2,16 @@ package bjfu.it.zhangsixuan.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrinkActivity extends AppCompatActivity {
 
@@ -16,19 +23,49 @@ public class DrinkActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drink);
 
         int drinkId = getIntent().getIntExtra(EXTRA_DRINKID, 0);
-        Drink drink = Drink.drinks[drinkId];
 
-        // coffee image
-        ImageView photo = findViewById(R.id.photo);
-        photo.setImageResource(drink.getImageResource());
-        photo.setContentDescription(drink.getName());
+        /*
+         * 先实例化helper，再获取数据库引用
+         */
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        /*
+         * try with resource 自动关闭数据库
+         */
+        try (SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase()) {
 
-        // name
-        TextView name = findViewById(R.id.name);
-        name.setText(drink.getName());
+            Cursor cursor = db.query("DRINK",
+                    new String[]{"NAME", "DESCRIPTION","IMAGE_SOURCE_ID"},
+                    "_id=?",
+                    new String[]{Integer.toString(drinkId)},
+                    null, null, null);
 
-        // description
-        TextView description = findViewById(R.id.description);
-        description.setText(drink.getDescription());
+            if (cursor.moveToFirst()){
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                int photoId = cursor.getInt(2);
+
+                // 显示name
+                TextView name = findViewById(R.id.name);
+                name.setText(nameText);
+
+                // 显示description
+                TextView description = findViewById(R.id.description);
+                description.setText(descriptionText);
+
+                // 显示photo
+                ImageView photo = findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nameText);
+            }
+            cursor.close();
+
+
+        } catch (SQLiteException e) {
+            Log.e("sqlite", e.getMessage());
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+
     }
 }
--- Java/Starbuzz/app/src/main/java/bjfu/it/zhangsixuan/starbuzz/DrinkCategoryActivity.java
+++ Java/Starbuzz/app/src/main/java/bjfu/it/zhangsixuan/starbuzz/DrinkCategoryActivity.java
@@ -3,32 +3,63 @@ package bjfu.it.zhangsixuan.starbuzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends AppCompatActivity {
+
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+        ListView listDrinks = findViewById(R.id.list_drinks);
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
 
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
-                this, //当前作用的对象
-                android.R.layout.simple_list_item_1,  //内建布局资源
-                Drink.drinks); //数据源
+        try (SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase()) {
+            /*
+             * selection 的位置是null
+             */
+            cursor = db.query("DRINK", new String[]{"_id", "NAME"}, null,
+                    null, null, null, null, null);
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(
+                    this,
+                    // 内置布局，每行显示一个文本框
+                    android.R.layout.simple_list_item_1,
+                    // 数据源
+                    cursor,
+                    // 显示name
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1},
+                    0
+            );
 
-        ListView listDirnks = findViewById(R.id.list_drinks);
-        listDirnks.setAdapter(listAdapter);
+            // 指定适配器
+            listDrinks.setAdapter(listAdapter);
 
-        // 监听器
+        } catch (SQLiteException e) {
+            Log.e("sqlite", e.getMessage());
+            Toast.makeText(this, "database unavailable", Toast.LENGTH_SHORT).show();
+        }
+
         OnItemClickListener itemClickListener = new OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> listDrinks,
+                                    View view,
+                                    int position,
+                                    long id) {
                 Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
                 intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
                 startActivity(intent);
@@ -36,8 +67,15 @@ public class DrinkCategoryActivity extends AppCompatActivity {
         };
 
         // 设置监听器
-        ListView listView = findViewById(R.id.list_drinks);
-        listView.setOnItemClickListener(itemClickListener);
+        listDrinks.setOnItemClickListener(itemClickListener);
+//        ListView listView = findViewById(R.id.list_drinks);
+//        listView.setOnItemClickListener(itemClickListener);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
     }
 }
